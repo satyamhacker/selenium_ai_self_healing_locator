894b43ffa0e4f14c2fb2f58da5f4afe04ad0b8b6
satyamhacker <satyamgrandmaster@gmail.com>
2026-04-29 05:53:11 +0530

    refactor(webdriver): implement synchronization for login page
    - Integrate WebDriverExtensions for element synchronization
    - Update username field locator to use data-test attribute
    - Modify element finding logic in enter_username, enter_password, and click_login methods
    - Add .gitignore to exclude driver files and Python cache

--- pages/sauce_login_page.py
+++ pages/sauce_login_page.py
@@ -1,18 +1,19 @@
 # pages/sauce_login_page.py (Current - Classical)
 from selenium.webdriver.common.by import By
+from utils.webdriver_extensions import WebDriverExtensions
 
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
-        self.username_field = (By.ID, "user-name")
+        self.username = (By.CSS_SELECTOR, "[data-test='wrong-username-locator']")
         self.password_field = (By.ID, "password")
         self.login_btn = (By.ID, "login-button")
     
     def enter_username(self, text):
-        self.driver.find_element(*self.username_field).send_keys(text)
+        WebDriverExtensions.AIfind_element_sync(self.driver, self.username[0].name, self.username[1]).send_keys(text)
     
     def enter_password(self, text):
-        self.driver.find_element(*self.password_field).send_keys(text)
+        WebDriverExtensions.AIfind_element_sync(self.driver, self.password_field[0].name, self.password_field[1]).send_keys(text)
     
     def click_login(self):
-        self.driver.find_element(*self.login_btn).click()
+        WebDriverExtensions.AIfind_element_sync(self.driver, self.login_btn[0].name, self.login_btn[1]).click()
