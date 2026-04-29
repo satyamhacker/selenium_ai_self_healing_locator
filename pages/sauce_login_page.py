58931948f708de57f7eaa1c21d74b089d5135a19
satyamhacker <satyamgrandmaster@gmail.com>
2026-04-29 08:29:38 +0530

    feat(sauce_login_page): add login functionality methods
    - Implement enter_username method to input username
    - Implement enter_password method to input password
    - Implement click_login method to submit login form

--- pages/sauce_login_page.py
+++ pages/sauce_login_page.py
@@ -6,3 +6,17 @@ class LoginPage:
         self.username = (By.ID, "user-name")
         self.password_field = (By.ID, "password")
         self.login_btn = (By.ID, "login-button")
+
+    def enter_username(self, text):
+        username_field = self.driver.find_element(*self.username)
+       
+        username_field.send_keys(text)
+
+    def enter_password(self, text):
+        password_field = self.driver.find_element(*self.password_field)
+      
+        password_field.send_keys(text)
+
+    def click_login(self):
+        login_button = self.driver.find_element(*self.login_btn)
+        login_button.click()
